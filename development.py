1237d5a82d22d75bc146dbb4919a5d12630c9e1a
nateGeorge <wordsforthewise@gmail.com>
2019-01-17 22:22:29 -0700

    a few lines

--- development.py
+++ development.py
@@ -31,3 +31,37 @@ create_follow_cluster_matrix(sp500, k)
 # get average values of candles in clusters
 sp500_norm['Cluster'] = labels
 sp500_norm.groupby('Cluster').mean()
+
+
+# try KNN
+from sklearn.neighbors import KNeighborsRegressor as KNN
+
+knn = KNN(k=5)  # default k
+
+
+import sys
+sys.path.append('../stock_prediction/code')
+
+import dl_quandl_EOD as dlq
+dfs = dlq.load_stocks()
+
+def get_open_normalised_prices_features_targets(dfs, symbol, start, end):
+    """
+    Obtains a pandas DataFrame containing open normalised prices
+    for high, low and close for a particular equities symbol
+    from Yahoo Finance. That is, it creates High/Open, Low/Open
+    and Close/Open columns.
+    """
+    df = dfs[symbol]
+    df['1d_pct_chg'] = df['Adj_Close'].pct_change()
+    df["H/O"] = df["Adj_High"]/df["Adj_Open"]
+    df["L/O"] = df["Adj_Low"]/df["Adj_Open"]
+    df["C/O"] = df["Adj_Close"]/df["Adj_Open"]
+    df.drop(
+        [
+            "Open", "High", "Low",
+            "Close", "Volume", "Adj Close"
+        ],
+        axis=1, inplace=True
+    )
+    return df
